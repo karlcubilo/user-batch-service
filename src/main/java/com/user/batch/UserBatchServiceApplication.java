4f9119def276f354357e9dea9715bbe043beb487
Karl Cubilo <karl.cubilo@gmail.com>
2024-08-22 19:47:37 +0800

    initial commit

---
+++ src/main/java/com/user/batch/UserBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.user.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBatchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/batch/UserBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
